876f3214912a61f760f297b95ab07918cd9e3d45
Xavier0999 <gustavoxavier0999@gmail.com>
2025-04-04 19:19:27 -0300

    Delete CadastroDeVotos/CadastroDeVotos directory

--- CadastroDeVotos/CadastroDeVotos/src/main/java/com/example/CadastroDeVotos/repository/RestauranteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.CadastroDeVotos.repository;
-
-import com.example.CadastroDeVotos.model.Funcionario;
-import com.example.CadastroDeVotos.model.Restaurante;
-import com.example.CadastroDeVotos.model.Votos;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RestauranteRepository extends JpaRepository<Restaurante, Integer> {
-}
